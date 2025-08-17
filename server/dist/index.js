f8d323d88da54a362bbcfa6892efbc340dc02947
NghiaTin <letatinnghia123@gmail.com>
2025-08-17 19:44:17 +0700

    another one

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,4 @@
-import { app } from "./app.js";
+import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 const PORT = process.env.PORT || 5000;
 // CONNECTION AND LISTSENERS
