226aa95b2270e0e1edb028e7d80a6fc3aed7021e
NghiaTin <letatinnghia123@gmail.com>
2025-08-17 21:13:47 +0700

    12

--- server/dist/app.js
+++ server/dist/app.js
@@ -18,9 +18,5 @@ app.use(express.json());
 // REMOVE THIS WHEN THIS APPLICATION IS MOVE TO PRODUCTION
 app.use(morgan("dev"));
 app.use("/api/v1", appRouter);
-// Routes ở đây
-app.get("/", (req, res) => {
-    res.send("Hello from Vercel!");
-});
 export default app;
 //# sourceMappingURL=app.js.map
