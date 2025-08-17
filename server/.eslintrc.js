1bf1764453ffc1bf4b93de8e9737f9b0c09c70f3
NghiaTin <letatinnghia123@gmail.com>
2025-08-17 20:39:50 +0700

    Create .eslintrc.js

---
+++ server/.eslintrc.js
@@ -0,0 +1,14 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
