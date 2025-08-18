04b42e1d5147902e2041997517efb7fbd0566cfe
NghiaTin <letatinnghia123@gmail.com>
2025-08-18 19:08:33 +0700

    1ssst

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // đảm bảo asset path đúng khi deploy
+  build: {
+    outDir: "dist", // mặc định là "dist", có thể để nguyên
+  },
+  server: {
+    port: 3000, // local dev port, optional
+  },
+});
